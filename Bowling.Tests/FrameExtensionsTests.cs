413b2701cc622bd5336457d2fa8ff6115bccb475
Felix Ruthenberg <felix.ruthenberg@generic.de>
2022-05-27 14:43:34 +0200

    IsSpareTests

--- Bowling.Tests/FrameExtensionsTests.cs
+++ Bowling.Tests/FrameExtensionsTests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using MoreLinq.Extensions;
 
 namespace Bowling.Tests;
 
@@ -7,10 +8,26 @@ public class FrameExtensionsTests
     [TestCase(9, false)]
     [TestCase(10, true)]
     [TestCase(11,false)]
-    public void IsLastShouldOnlyBeTrueForFrameNumber10(int frameNumber, bool expected)
+    public void IsLast(int frameNumber, bool expected)
     {
         var frame = new Frame{Number = frameNumber};
         var actual = frame.IsLast();
+        actual.Should().Be(expected);
+    }
+
+    [TestCase(new[]{1, 3}, false)]
+    [TestCase(new[]{7, 2}, false)]
+    [TestCase(new[]{7, 3}, true)]
+    [TestCase(new[]{4, 6}, true)]
+    [TestCase(new[]{4}, false)]
+    [TestCase(new[]{10}, false)]
+    [TestCase(new int[]{}, false)]
+    [TestCase(new[]{2,5,3}, false)]
+    public void IsSpare(int[] pins, bool expected)
+    {
+        var frame = new Frame();
+        pins.ForEach(frame.Pins.Add);
+        var actual = frame.IsSpare();
         actual.Should().Be(expected);
     }
 }
