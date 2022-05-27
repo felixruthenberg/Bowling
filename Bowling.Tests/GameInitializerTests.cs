8324b2df400f060e26771f6917a676c093e5f778
Felix Ruthenberg <felix.ruthenberg@generic.de>
2022-05-28 01:20:30 +0200

    add GameInitializerTests

---
+++ Bowling.Tests/GameInitializerTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace Bowling.Tests;
+
+public class GameInitializerTests
+{
+    [Test]
+    public void CreateFrameShouldInitializeFrames()
+    {
+        var actual = GameInitializer.CreateFrames();
+
+        using var _ = new AssertionScope();
+        actual.Should().HaveCount(10);
+        actual[0].Number.Should().Be(1);
+        actual[^1].Number.Should().Be(10);
+        actual.Should().BeInAscendingOrder(f => f.Number);
+    }
+}
