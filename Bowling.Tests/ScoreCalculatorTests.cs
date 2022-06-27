207d1ff2de5c8726e71485d17748b700a6d3334b
Felix Ruthenberg <felix.ruthenberg@generic.de>
2022-06-27 22:49:25 +0200

    implement ScoreCalculatorTests

--- Bowling.Tests/ScoreCalculatorTests.cs
+++ Bowling.Tests/ScoreCalculatorTests.cs
@@ -1,25 +1,34 @@
-﻿using MoreLinq.Extensions;
+﻿using FluentAssertions;
+using MoreLinq.Extensions;
 
 namespace Bowling.Tests;
 
 public class ScoreCalculatorTests
 {
-    [TestCase(new[] {1, 3}, new int[] { }, 4)]
-    [TestCase(new[] {1, 3}, new[] {10}, 4)]
-    [TestCase(new[] {10}, new[] {10}, 20)]
-    [TestCase(new[] {10}, new[] {10, 10}, 30)]
-    [TestCase(new[] {10}, new[] {10, 10, 10}, 30)]
-    [TestCase(new[] {0}, new int[] { }, 0)]
-    [TestCase(new[] {0, 10}, new[] {1, 2}, 11)]
-    [TestCase(new[] {10}, new[] {0, 8}, 18)]
-    public void CalculateFrameScore(int[] frameRolls, int[] nextRolls, int expected)
+    [TestCase(new[] {1, 3}, new int[] { }, new int[] { }, 4, 4)]
+    [TestCase(new[] {1, 3}, new[] {10}, new int[] { }, 4, 14)]
+    [TestCase(new[] {10}, new[] {10}, new int[] { }, 20, 30)]
+    [TestCase(new[] {10}, new[] {10}, new[] {10}, 30, 50)]
+    [TestCase(new[] {0}, new int[] { }, new int[] { }, 0, 0)]
+    [TestCase(new[] {0, 10}, new[] {1, 2}, new int[] { }, 11, 14)]
+    [TestCase(new[] {10}, new[] {0, 8}, new int[] { }, 18, 26)]
+    public void CalculateFrameScore(int[] firstFrameRolls,
+        int[] secondFrameRolls,
+        int[] thirdFrameRolls,
+        int expectedFirstFrameScore, int expectedSecondFrameScore)
     {
-        var frame = new Frame();
-        frameRolls.ForEach(frame.Rolls.Add);
-        // TODO: Tests
-        Assert.Fail(" // TODO: Tests");
-        // var actual = ScoreCalculator.CalculateScores(frame, nextRolls);
+        var firstFrame = new Frame {Number = 1};
+        firstFrameRolls.ForEach(firstFrame.Rolls.Add);
 
-        // actual.Should().Be(expected);
+        var secondFrame = new Frame {Number = 2};
+        secondFrameRolls.ForEach(secondFrame.Rolls.Add);
+
+        var thirdFrame = new Frame {Number = 3};
+        thirdFrameRolls.ForEach(thirdFrame.Rolls.Add);
+
+        ScoreCalculator.CalculateScores(new[] {firstFrame, secondFrame, thirdFrame});
+
+        firstFrame.TotalScore.Should().Be(expectedFirstFrameScore);
+        secondFrame.TotalScore.Should().Be(expectedSecondFrameScore);
     }
 }
